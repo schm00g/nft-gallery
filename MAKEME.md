6b540d2c1321cc13bebe1eb2017d695960afbd6b
Your Actual Name <Your Actual Email>
2021-12-08 22:40:19 -0500

    init commit

---
+++ MAKEME.md
@@ -0,0 +1,34 @@
+# Notes
+
+Some ideas to experiment with:
+
+### Layout
+
+- Horizontal scroll like walking around a gallery | map?
+- Once piece at a time centre foreground with left right cutoff
+- Metadata fades in corners as you scroll/hover over
+- mobile
+- QR code for metadata on phone, landscape Artwork Labels
+- aspect ratio for different sizes
+
+### Inspiration:
+- https://www.off---white.com/en-gb/shopping/man/clothing
+- https://www.glauber.xyz/
+
+### Loading
+
+- IPFS grabbing from there will be slow
+- Support ETH, SOL, TEZ wallet addresses
+- Store images locally? Automate grabbing from wallet to local repo
+- Do I connect to the wallet or the blockchain?
+- blocklist to hide certain NFTs
+- will the wallet addresses remain in a local config? encrypted?
+- loading spinner
+- How to display videos
+
+### Hosting
+- metacase.space HOSTINGER
+- page routing §index.html§
+
+### Repo
+- git private repo
---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="container">
+      <div className="horizontal-scroll-wrapper">
+          <img src="https://ipfs.io/ipfs/QmPTDyQHrY8j16fn4UT13SaMXazC2tSrDigkaoSs4gVgki" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmcmrMQTHfRrjW3DPpbwNgRrxPhEHVSknP3BPqyy7ZR3ME" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmSTa46RVebhotBXZsM1EfUHYDJCLU4XivXMY6QpRHqp9n" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmQhAzrECZbZmx8trxgfQCbWgsUQhjmePagevNnbmBtDap" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmRz6kmaj74h7FYngmZoZGf6UpedihJJxdkEQAppZL5jzT" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmQiC4ekvBCUqERnpkkqhiqyjFq6VAM25qfPuFpA1z7bV9" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmWvjzJzdy52purZW4FMGjxtVb6z4PnytPq9bcBt1turSC" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmU8ccbcZRz1GzqTHKkQfBLSU8o1UBDPZrX1JZ8fknTtbU" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/Qmanq6k9NMRZNqaCHEVej1PFoNNbXw9CwcTLsb6SvzR3mD" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmZAEnxrWCvdzT6fsrwuRQGRHMxAqqMSz1bw4qzVKXr1Jq" className="art" alt="logo" />
+          <img src="https://lh3.googleusercontent.com/jM3_eKUj2tk5Diq4oD9xlEvY9wW0Vz0j4ybyNa-0d4qiHUkGlko4czkZaRbvOfD4MUgVk8raWhlHUyLhlAw2gGF-XxcY-KBm4xLBHA=s0" className="art" alt="logo" />
+          <img src="https://lh3.googleusercontent.com/8KvLBL_pa-TgH7Gfd8wwmpuDa8staGx3frRZY9iOM2nyYQ8dQMFAn_WTvadIaS0hlSzEET_ELo8pK8bisZnbYT53hOdKqKIuiV6n8w=s0" className="art" alt="logo" />
+          <img src="https://lh3.googleusercontent.com/3U0VLsOh3t8yYRw2qDtz64qBgRlumHJnCx-xrkwmBcKGGHMKGk2c1wtCFOi_srpWM-qO8TBVbA8jbj_NdjbFn6E1r7WJETp797ZI=s0" className="art" alt="logo" />
+          <video width="240" height="240" autoPlay><source src="https://d2ybmb80bbm9ts.cloudfront.net/fH/Ns/QmSotq1BkUof6a3codWzhYsV6HbjFVDj5ZnXNCxw6VfHNs/nft_q4.mp4" type="video/mp4"></source></video>
+          <video width="240" height="240" autoPlay><source src="https://ipfs.io/ipfs/QmSRkpUX2Nukg83ChPvT2BcQ1wWK16nWkwgu8gxR1tBedE" type="video/mp4"></source></video>
+          <img src="https://www.arweave.net/dvt-y2oJ4Rt6Nc6a-ym2VnSHRjWRLyQD8Ggfy-We8rw?ext=png" className="art" alt="logo" />
+          <img src="https://www.arweave.net/4h5LaM0uegmZ-0Xe2TzDw5w38q1ecPzKNspqBiADXPs?ext=PNG" className="art" alt="logo" />
+          <img src="https://www.arweave.net/NKkXxHJPlPKS6zzOXClhEzMi5UEOM7sKuaox_Ae-RGI?ext=png" className="art" alt="logo" />
+          <img src="https://www.arweave.net/uwfoqr0zFCtQlGGArvs21Wsaq8m6jGJEAjBBqD8JgLo?ext=PNG" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmNi5KHKTGECpcpvJoYr9drVDYHZTh8MWrCJZAbxuRKx3W" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/bafybeicod5lavzlfvdd6pbhzbfj36bgywfm6hgs36hwz4jg7ukh73ert7a" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmVDb8XdSxHVwYFJjDA54akfkkAU2ZtG6PAxwDCBAugAqb" className="art" alt="logo" />
+          <img src="https://ipfs.io/ipfs/QmTXTgrz8zqK5462D5esVTEeEV8rdbryu2cWjRsf4S7SHL" className="art" alt="logo" />
+          <video width="240" height="240" autoPlay><source src="https://ipfs.io/ipfs/QmRHLJoTG63JZ5expAKN81obodtxeAXoWv5keJNZ329Hn2" type="video/mp4"></source></video>
+          <video width="240" height="240" autoPlay><source src="https://ipfs.io/ipfs/QmXEVjduTUZkKnf67zudEPAwi7CiCMmdKdVHpACJ68tQqn" type="video/mp4"></source></video>
+          <img src="https://ipfs.io/ipfs/QmZ2CXGsV5eXauzq8zg179vMTUC6DJM15QCrf1p7tUJDfh" className="art" alt="logo" /> 
+        </div>
+      <div className="metadata">
+      <p>
+          <code>NFT Gallery</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://www.hicetnunc.xyz/tz/tz1U2E28joVrLzg2rYcPaon6w8b4QtsoiQcL/collection"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TEZ
+        </a>
+        <a
+          className="App-link"
+          href="https://rainbow.me/0x98E579c0221eB16Ae66A11D10fb5e5D1e245Ec0f"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ETH
+        </a>
+        <a
+          className="App-link"
+          href="https://nfteyez.global/accounts/A5GXAB7Lmf1VcWgrBxqw1VD3mpG6ZzfTYjrVSVoWjWjR"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SOL
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
